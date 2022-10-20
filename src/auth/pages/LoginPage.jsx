dd7da1d781a991c9254eca35b2509ede5d2bc8a9
lanzarot <luis-389@hotmail.com>
2022-10-20 15:16:45 -0500

    LoginScreen and Navbar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,78 @@
-
+import './LoginPage.css'
 
 export const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Login to <strong>Calendar App</strong>📆</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input 
+                                type="text"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="form-group mb-2 text-center">
+                            <input 
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login" 
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Sign up</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Full name"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password" 
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Confirm password" 
+                            />
+                        </div>
+
+                        <div className="form-group mb-2 text-center">
+                            <input 
+                                type="submit" 
+                                className="btnSubmit" 
+                                value="Sign up" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
