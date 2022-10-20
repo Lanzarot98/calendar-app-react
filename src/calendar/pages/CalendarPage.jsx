ddfeb65b064db964b55ccc69234a22e682b39592
lanzarot <luis-389@hotmail.com>
2022-10-20 16:22:13 -0500

    React Big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,51 @@
-import { Navbar } from "../";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import enUS from 'date-fns/locale/en-US';
+
+import { Navbar } from "../";
+import { addHours, parse, startOfWeek, getDay, format } from 'date-fns';
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'birthday chief',
+  notes: 'Have to buy the cake',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Luis Miguel'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      
+      <div>
+        <Calendar
+          localizer={ localizer }
+          events={ events }
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 'calc(100vh - 80px)' }}
+        />
+      </div>
+
+
     </>
   )
 }
